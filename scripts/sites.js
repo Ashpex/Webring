71aa1f8f38cc465b0223199d94de25505cfde9c8
Ashpex <38400042+Ashpex@users.noreply.github.com>
2020-03-10 22:29:28 +0700

    add comma

--- scripts/sites.js
+++ scripts/sites.js
@@ -855,6 +855,14 @@ const sites = [
     title: 'jenett. simply. personal.',
     type: 'blog',
     url: 'https://simply.personal.jenett.org'
+  },
+  
+  {
+    author: 'ashpex',
+    langs: ['en'],
+    title: '~/ashpex',
+    type: 'blog',
+    url: 'https://ashpex.neocities.org'
   }
 ]
 
